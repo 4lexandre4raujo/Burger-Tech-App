7ff9fecbf0ba4d642cb7497ddeb082b0a1b8df56
Alexandre Araújo <alexandresaraujo19@gmail.com>
2022-09-15 17:06:43 -0300

    add firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,25 @@ import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
+    AngularFireModule.initializeApp({ 
+      apiKey: "AIzaSyAeckrorv7nEmcoe2SMzCjsO2AIWfPCyto",
+      authDomain: "burger-tech-app.firebaseapp.com",
+      projectId: "burger-tech-app",
+      storageBucket: "burger-tech-app.appspot.com",
+      messagingSenderId: "344322130765",
+      appId: "1:344322130765:web:40a5b8130f06eafc112d04"
+     }),
+     AngularFirestoreModule
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
